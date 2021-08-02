31dd07cd8c4d605b6b0f8f00eb8cc24050d9c159
Corentin-Robac <corentin.robac@laposte.net>
2021-08-02 20:29:17 +0200

    Routes

--- back/app.js
+++ back/app.js
@@ -1,24 +1,26 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
+//Lier la bdd
+mongoose.connect('mongodb+srv://Crob:nUEFO09Dlqxpr78S@cluster0.qxjpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//Headers
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+//Utiliser le router
+app.use('/api/sauces', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
